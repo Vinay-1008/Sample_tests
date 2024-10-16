1276bc89220ad2b647f80e16d12255c9ffc016a2
Vinay <vinayreddy100800@gmail.com>
2024-10-16 15:17:42 +0530

    adding sample test 2

--- utils/test_application_utils.py
+++ utils/test_application_utils.py
@@ -45,3 +45,5 @@ def delete_object():
         print(f"Delete object failed to delete with the error {response.json()['error']}")
     return response.json()
 
+def test_sample_test_2():
+    assert True
